007663bfa015db0c5fc14014bf61d54425f9b452
woopaca <jcw001031@gmail.com>
2022-01-16 16:26:16 +0900

    initial commit

---
+++ index.py
@@ -0,0 +1,18 @@
+dict_first = {'사과': 30, '배': 15, '감': 10, '포도': 10}
+dict_second = {'사과': 5, '감': 25, '배': 15, '귤': 25}
+
+
+def merge_dict(dict_first, dict_second):
+    arr=['감', '귤', '배', '사과', '포도']
+    dict_total={}
+    for i in arr:
+        if dict_first.get(i) and dict_second.get(i):
+            dict_total[i]=dict_total[i]=dict_first.get(i) + dict_second.get(i)
+        else:
+            if dict_first.get(i):
+                dict_total[i]=dict_first.get(i)
+            else:
+                dict_total[i]=dict_second.get(i)
+    print(dict_total)
+    
+merge_dict(dict_first, dict_second)
