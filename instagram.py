7f35c8cb456f20e6bdee92cc4b04e727018b9dd8
woopaca <jcw001031@gmail.com>
2022-05-27 19:31:04 +0900

    modify 'instagram.py' and upgrade chromedriver

--- instagram.py
+++ instagram.py
@@ -6,13 +6,27 @@ import time
 driver = webdriver.Chrome('/Users/jcw/Coding/Python/chromedriver')
 driver.get('https://instagram.com')
 
-time.sleep(4)
+time.sleep(2)
 
 e = driver.find_elements_by_class_name('_2hvTZ')[0]
-e.send_keys('jcw001031@gmail.com')
+e.send_keys('jcw001031@icloud.com')
 
 e = driver.find_elements_by_class_name('_2hvTZ')[1]
-e.send_keys('Chan!10571057 ')
+e.send_keys('Chan!1057')
 e.send_keys(Keys.ENTER)
 
-PZuss _7UhW9
+time.sleep(3)
+
+e = driver.find_elements_by_class_name('sqdOP')[0]
+e.send_keys(Keys.ENTER)
+
+time.sleep(3)
+
+e = driver.find_element_by_class_name('HoLwm')
+e.send_keys(Keys.ENTER)
+
+e = driver.find_element_by_class_name('gmFkV')
+e.send_keys(Keys.ENTER)
+
+e = driver.find_element_by_class_name('_9AhH0')
+e.send_keys(Keys.ENTER)
